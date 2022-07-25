8a9c8a4b339e1c79fd9eccfdf467eda9a07e18d3
Dan Pariente Hachuel <61590061+DanHachuel@users.noreply.github.com>
2022-07-24 22:32:48 -0300

    Create comparador.c
    comparador de numeros

---
+++ comparador.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n1 = 0;
+    int n2 = 0;
+    printf("\n Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("\n Digite o segundo numero: ");
+    scanf("%d", &n2);
+    printf("\n n1 e n2 sao iguais? %d", n1==n2);
+    printf("\n n1 e maior que n2? %d", n1>n2);
+    printf("\n n1 e menor ou igual a n2? %d", n1<=n2);
+}
