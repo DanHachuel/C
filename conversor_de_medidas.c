914d348df391f2e8956eb78a80a09a8a1cc21299
Dan Pariente Hachuel <61590061+DanHachuel@users.noreply.github.com>
2022-08-08 10:43:25 -0300

    Create conversor_de_medidas.c
    conversor de metros em centímetros

---
+++ conversor_de_medidas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main () { 
+	float metros, converte = 0; 
+	printf("insire um valor em metros para ser convertido:"); 
+	scanf("%f", &metros); converte = metros*100; 
+	printf("o valor em centímetros %f: \n", converte);
+ 
+return 0;
+ 
+}
